8dd1dfcd129a5afc3a24aa883862226a75809bbe
nekora <n3k0rA@hotmail.com>
2013-01-09 20:34:51 +0100

    emailWorking

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,19 @@ Hikultura::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => 'hik-on.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'hik-online.heroku.com' }
+  
+  
+  config.action_mailer.default_url_options = { :host =>"www.hikultura.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => 'info@hikultura.com',
+    :password             => ENV['GMAIL_SMTP_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+    
   
 end
