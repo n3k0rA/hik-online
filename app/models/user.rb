1735b51b2bb02f3caed323cc08d2de92086faab7
nekora <n3k0rA@hotmail.com>
2013-01-13 14:02:42 +0100

    s

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
      },
-    :bucket => ENV['S3_BUCKET_NAME'],
+    :bucket => ENV['S3_BUCKET_NAME']
   
   
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
