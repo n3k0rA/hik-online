bcfe42a1c91b6a9d733bb3b882c53bd4662a655c
nekora <n3k0rA@hotmail.com>
2013-01-13 13:46:16 +0100

    s

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,7 @@ class User < ActiveRecord::Base
   
   #Paperclip
   #validates_attachment_size :avatar, :less_than => 5.megabytes    
-  has_attached_file :avatar,
-  :styles => { :original => "1024x1024>", :medium => "300x300>", :thumb => "100x100>" },
-  :storage =>  Rails.env.production? ? :s3 : :filesystem,
-  :s3_credentials => {
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-   },
-  :path => ":attachment/:id/:style.:extension",
-  :bucket => ENV['S3_BUCKET_NAME'],
-  :default_url => '/images/missing_:style.jpg'
+  has_attached_file :avatar
   
   
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
